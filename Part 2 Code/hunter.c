4b9ab81e36d35b50221ef7dee6fa24f3198ab918
Jeffery-Lee-QAQ <66355351+Jeffery-Lee-QAQ@users.noreply.github.com>
2020-08-05 21:07:18 +0800

    update hunter.c

--- Part 2 Code/hunter.c
+++ Part 2 Code/hunter.c
@@ -20,206 +20,93 @@ void decideHunterMove(HunterView hv)
 {
 	Round cur_round = HvGetRound(hv);
 	Player cur_player = HvGetPlayer(hv);
-	PlaceId cur_palce = HvGetPlayerLocation(hv, cur_player);
+	PlaceId cur_place = HvGetPlayerLocation(hv, cur_player);
 	int hp = HvGetHealth(hv, cur_player);
 
 	if (cur_player == PLAYER_LORD_GODALMING) {				
 		if (cur_round == 0) {
-			char *xx = "GR";
-			registerBestPlay(xx, "Have we nothing Toulouse?");		
-		} else {
-			// if hunter hp is less than 5
-			// stay here
-			if (hp < 5) {
-				char *xx = placeIdToAbbrev(cur_palce);
-				registerBestPlay(xx, "Have we nothing Toulouse?");	
-			}
-			// patrol area
-			PlaceId patrol[15] = {CADIZ, LISBON, MADRID, GRANADA, ALICANTE,
-				SARAGOSSA, SANTANDER, BARCELONA, BORDEAUX, TOULOUSE, CAGLIARI,
-				MARSEILLES, NANTES, MEDITERRANEAN_SEA, CLERMONT_FERRAND}; 
-			
-			// an array to collect all best moves for hunter
-			PlaceId best_moves[MAX_REAL_PLACE];
-			int count = 0;
-			
-			// get the hunter reachable places
-			int numcanGo = 0; 
-			PlaceId *canGo = HvWhereCanIGo(hv, &numcanGo);
-			// remove the current palce
-			placesDelete(canGo, numcanGo, cur_palce);
-			for (int i = 0; i < 15; i++) {
-				if (placesContains(canGo, numcanGo, patrol[i])) {
-					best_moves[count] = patrol[i];
-					count++;
-				}
-			}
-			// randomly pick a best move frome array
-			PlaceId next_move = best_moves[cur_round % count];
-			char *xx = placeIdToAbbrev(next_move);
-			registerBestPlay(xx, "Have we nothing Toulouse?");		
-		}
-
-	}
-	
-	if (cur_player == PLAYER_DR_SEWARD) {
-		if (cur_round == 0) {
-			char *xx = "SZ";
-			registerBestPlay(xx, "Have we nothing Toulouse?");		
-		} else {
-			// if hunter hp is less than 5
-			// stay here
-			if (hp < 5) {
-				char *xx = placeIdToAbbrev(cur_palce);
-				registerBestPlay(xx, "Have we nothing Toulouse?");	
-			}
-			// an array to collect all best moves for hunter
-			PlaceId best_moves[MAX_REAL_PLACE];
-			int count = 0;
-
-			// find drac reachable places
-			int numLocs = 0;
-			PlaceId *locs = HvWhereCanTheyGo(hv, PLAYER_DRACULA, &numLocs);
-			// if find
-			if (locs != NULL) {
-				// get the hunter reachable places
-				int numcanGo = 0; 
-				PlaceId *canGo = HvWhereCanIGo(hv, &numcanGo);
-				for (int i = 0; i < numcanGo; i++) {
-					if (placesContains(locs, numLocs, canGo[i])) {
-						best_moves[count] = canGo[i];
-						count++;
-					}
-				}
-				// randomly pick a best move frome array
-				PlaceId next_move = best_moves[cur_round % count];
-				char *xx = placeIdToAbbrev(next_move);
-				registerBestPlay(xx, "Have we nothing Toulouse?");				
-			}
-
-			// search the last plays
-			int drac_round = 0;
-			PlaceId drac_find = HvGetLastKnownDraculaLocation(hv, &drac_round);
-			// if find drac's trail is known 
-			// find the shorest path to this place
-			if (drac_find != NOWHERE) {
-				int length = 0;
-				PlaceId *path = HvGetShortestPathTo(hv, cur_player, drac_find, &length);
-				PlaceId next_move = path[0];
-				char *xx = placeIdToAbbrev(next_move);
-				registerBestPlay(xx, "Have we nothing Toulouse?");					
-			}
-			
-			// normal move
-			int numcanGo = 0; 
-			PlaceId *canGo = HvWhereCanIGo(hv, &numcanGo);
-			// remove the current paces 
-			if (placesContains(canGo, numcanGo, cur_palce)) {
-				placesDelete(canGo, numcanGo, cur_palce);				
-			}			
-			PlaceId next_move = canGo[cur_round % numcanGo];
-			char *xx = placeIdToAbbrev(next_move);
-			registerBestPlay(xx, "Have we nothing Toulouse?");	
-		}
-	}	
-	
-	if (cur_player == PLAYER_VAN_HELSING) {
-		if (cur_round == 0) {
 			char *xx = "GW";
-			registerBestPlay(xx, "Have we nothing Toulouse?");		
-		} else {
-			if (hp < 5) {
-				char *xx = placeIdToAbbrev(cur_palce);
-				registerBestPlay(xx, "Have we nothing Toulouse?");	
-			}
-			// patrol area
-			PlaceId patrol[14] = {GALWAY, DUBLIN, IRISH_SEA, LIVERPOOL, 
-				SWANSEA, LONDON, MANCHESTER, ENGLISH_CHANNEL, EDINBURGH,
-				PLYMOUTH, AMSTERDAM, NORTH_SEA, LE_HAVRE, BRUSSELS}; 
-			
-			// an array to collect all best moves for hunter
-			PlaceId best_moves[1000];
-			int count = 0;
-			
-			// get the hunter reachable places
-			int numcanGo = 0; 
-			PlaceId *canGo = HvWhereCanIGo(hv, &numcanGo);
-			// remove the current palce
-			placesDelete(canGo, numcanGo, cur_palce);
-			for (int i = 0; i < 14; i++) {
-				if (placesContains(canGo, numcanGo, patrol[i])) {
-					best_moves[count] = patrol[i];
-					count++;
-				}
-			}
-			// randomly pick a best move frome array
-			PlaceId next_move = best_moves[cur_round % count];
-			char *xx = placeIdToAbbrev(next_move);
-			registerBestPlay(xx, "Have we nothing Toulouse?");					
-		}
+			registerBestPlay(xx, "Wo si yi ge BAOAN!");		
+		} else {			
+			// search the last plays
+			int drac_round = 0;
+			PlaceId drac_find = HvGetLastKnownDraculaLocation(hv, &drac_round);
 
-	}	
-	
-	if (cur_player == PLAYER_MINA_HARKER) {
-		if (cur_round == 0) {
-			char *xx = "FR";
-			registerBestPlay(xx, "Have we nothing Toulouse?");		
-		} else {
-			// if hunter hp is less than 5
-			// stay here
-			if (hp < 5) {
-				char *xx = placeIdToAbbrev(cur_palce);
-				registerBestPlay(xx, "Have we nothing Toulouse?");	
-			}
-			// an array to collect all best moves for hunter
-			PlaceId best_moves[1000];
-			int count = 0;
-
-			// find drac reachable places
-			int numLocs = 0;
-			PlaceId *locs = HvWhereCanTheyGo(hv, PLAYER_DRACULA, &numLocs);
-			// if find
-			if (locs != NULL) {
+			if (hp < 5 || drac_find == cur_place) {
+				char *xx = placeIdToAbbrev(cur_place);
+				registerBestPlay(xx, "LEI TING GA BA!");	
+			} else if (cur_round > 6 && drac_find == NOWHERE) {
+				char *xx = placeIdToAbbrev(cur_place);
+				registerBestPlay(xx, "LEI TING GA BA!");				
+			} else {
+				// patrol area
+				PlaceId patrol[14] = {GALWAY, DUBLIN, IRISH_SEA, LIVERPOOL, 
+					SWANSEA, LONDON, MANCHESTER, ENGLISH_CHANNEL, EDINBURGH,
+					PLYMOUTH, AMSTERDAM, NORTH_SEA, LE_HAVRE, BRUSSELS}; 
+				
+				// an array to collect all best moves for hunter
+				PlaceId best_moves[MAX_REAL_PLACE];
+				int count = 0;
+				
 				// get the hunter reachable places
 				int numcanGo = 0; 
 				PlaceId *canGo = HvWhereCanIGo(hv, &numcanGo);
+				// remove the current palce
+				placesDelete(canGo, numcanGo, cur_place);
 				for (int i = 0; i < numcanGo; i++) {
-					if (placesContains(locs, numLocs, canGo[i])) {
+					if (placesContains(patrol, 14, canGo[i])) {
 						best_moves[count] = canGo[i];
 						count++;
 					}
 				}
 				// randomly pick a best move frome array
-				PlaceId next_move = best_moves[cur_round % count];
+				PlaceId next_move = best_moves[(cur_round - 1) % count];
 				char *xx = placeIdToAbbrev(next_move);
-				registerBestPlay(xx, "Have we nothing Toulouse?");				
+				registerBestPlay(xx, "Xun Luo zhong...");					
 			}
-
-			// search the last plays
-			int drac_round = 0;
-			PlaceId drac_find = HvGetLastKnownDraculaLocation(hv, &drac_round);
+		}
+	} else {
+		// search the last plays
+		int drac_round = 0;
+		PlaceId drac_find = HvGetLastKnownDraculaLocation(hv, &drac_round);
+		if (cur_round == 0) {
+			if (cur_player == PLAYER_DR_SEWARD) {
+				registerBestPlay("SZ", "GIAO GIAO!");					
+			} else if (cur_player == PLAYER_VAN_HELSING) {
+				registerBestPlay("MA", "GIAO GIAO!");	
+			} else {
+				registerBestPlay("FR", "GIAO GIAO!");	
+			}
+		} else if (hp < 5 || drac_find == cur_place) {			
+			// if hunter hp is less than 5
+			// stay here
+				char *xx = placeIdToAbbrev(cur_place);
+				registerBestPlay(xx, "LEI TING GA BA!");
+		} else if (drac_find != NOWHERE) {
 			// if find drac's trail is known 
 			// find the shorest path to this place
-			if (drac_find != NOWHERE) {
-				int length = 0;
-				PlaceId *path = HvGetShortestPathTo(hv, cur_player, drac_find, &length);
-				PlaceId next_move = path[0];
-				char *xx = placeIdToAbbrev(next_move);
-				registerBestPlay(xx, "Have we nothing Toulouse?");					
-			}
-			
+			int length = 0;
+			PlaceId *path = HvGetShortestPathTo(hv, cur_player, drac_find, &length);
+			PlaceId next_move = path[0];
+			char *xx = placeIdToAbbrev(next_move);
+			registerBestPlay(xx, "Bie pao, MEI MEI!");
+
+		} else if (cur_round > 6 && drac_find == NOWHERE) {
+				char *xx = placeIdToAbbrev(cur_place);
+				registerBestPlay(xx, "Yan jiu Yan jiu!");				
+		} else {				
 			// normal move
 			int numcanGo = 0; 
 			PlaceId *canGo = HvWhereCanIGo(hv, &numcanGo);
 			// remove the current paces 
-			if (placesContains(canGo, numcanGo, cur_palce)) {
-				placesDelete(canGo, numcanGo, cur_palce);				
+			if (placesContains(canGo, numcanGo, cur_place)) {
+				placesDelete(canGo, numcanGo, cur_place);				
 			}			
-			PlaceId next_move = canGo[cur_round % numcanGo];
+			PlaceId next_move = canGo[(cur_round - 1) % numcanGo];
 			char *xx = placeIdToAbbrev(next_move);
-			registerBestPlay(xx, "Have we nothing Toulouse?");	
-		}	
-	}	
-
+			registerBestPlay(xx, "HUO HUO!");		
+		}
+				
+	}
 
 }
